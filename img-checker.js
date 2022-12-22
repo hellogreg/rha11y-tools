b7c4465233a63b02b40bbd82fbdad1160a5f8b3d
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2022-12-22 15:01:39 -0600

    testing dir() of imgs

--- img-checker.js
+++ img-checker.js
@@ -6,16 +6,23 @@ javascript: (() => {
     el.style.setProperty("outline", color + " solid 8px", "important");
   }
 
+  // Get all non-shadow imgs
+  //
   const imgs = document.querySelectorAll("img");
   for (const img of imgs) {
     highlightElement(img, "#f90");
+    dir(img);
   }
 
+  // Get all non-shadow svgs
+  //
   const svgs = document.querySelectorAll("svg");
   for (const svg of svgs) {
     highlightElement(svg, "#ff0");
   }
 
+  // Get every img and svg that's in a shadowRoot.
+  //
   var nodes = document.querySelectorAll("*");
   for (const node of nodes) {
     if (node.shadowRoot) {
