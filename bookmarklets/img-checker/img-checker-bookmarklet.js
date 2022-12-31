c5dc538dc431591c5b0d76e819e0a9dff3eb6210
hellogreg <642695+hellogreg@users.noreply.github.com>
2022-12-31 11:48:16 -0600

    renaming files

--- bookmarklets/img-checker/img-checker-bookmarklet.js
+++ bookmarklets/img-checker/img-checker-bookmarklet.js
@@ -1,5 +1,8 @@
 javascript: (function () {
   var bookletCode = document.createElement("script");
-  bookletCode.setAttribute("src", "https://rha11y-tools.netlify.app/img-checker.js");
+  bookletCode.setAttribute(
+    "src",
+    "https://rha11y-tools.netlify.app/bookmarklets/img-checker/img-checker.js"
+  );
   document.body.appendChild(bookletCode);
 })();
