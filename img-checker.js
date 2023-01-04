3cb0c11186c9545e6fe62605e41ec29f0f5f14ec
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2023-01-04 15:46:26 -0600

    dir shadow children

--- img-checker.js
+++ img-checker.js
@@ -268,7 +268,7 @@ javascript: (() => {
       for (const node of nodes) {
         const shadowChild = node.shadowRoot;
         if (shadowChild) {
-          dir(shadowChild);
+          console.dir(shadowChild);
           log(
             "Found a shadow child (nesting level " +
               i +
