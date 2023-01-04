231575e4dff87ba4280e2fefc1af736c5abfbfbd
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2023-01-04 15:27:26 -0600

    checking parent hrefs

--- img-checker.js
+++ img-checker.js
@@ -102,7 +102,7 @@ javascript: (() => {
       while (!hid && parent && parent.nodeName !== "BODY" && parent.nodeName) {
         log("PARENT");
         if (parent.tagName && parent.tagName === "A") {
-          log("LINKY!");
+          log("LINK: " + parent.href);
         }
         console.dir(parent);
         hid = hid || isHidden(parent);
