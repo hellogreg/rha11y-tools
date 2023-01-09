0c1330c40b82a98634b70561225fd226d7916639
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2023-01-09 08:37:39 -0600

    Deleting and renaming test files

--- shadowfinder-bookmarklet.js
+++
@@ -1,5 +0,0 @@
-javascript: (function () {
-  var bookletCode = document.createElement("script");
-  bookletCode.setAttribute("src", "http://127.0.0.1:5500/shadowfinder.js");
-  document.body.appendChild(bookletCode);
-})();
