c50ad9a141943544ee58733ca1d3b8d5d10dbc85
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2023-01-11 14:41:19 -0600

    Renamed img-hide

--- img-hide-bookmarklet.js
+++ img-hide-bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {
   var bookletCode = document.createElement("script");
-  bookletCode.setAttribute("src", "https://rha11y-img.netlify.app/img-hider.js");
+  bookletCode.setAttribute("src", "https://rha11y-img.netlify.app/img-hide.js");
   document.body.appendChild(bookletCode);
 })();
