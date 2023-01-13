1f47642074c46a9b6f119360f1363a866b046344
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2023-01-13 12:52:34 -0600

    cleaning up src, CSS classes, and inline styles

--- link-target-bookmarklet.js
+++ link-target-bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {
-  var bookletCode = document.createElement("script");
-  bookletCode.setAttribute("src", "https://rha11y-tools.netlify.app/link-target.js");
-  document.body.appendChild(bookletCode);
+  var booklink = document.createElement("script");
+  booklink.setAttribute("src", "https://rha11y-tools.netlify.app/link-target.js");
+  document.body.appendChild(booklink);
 })();
