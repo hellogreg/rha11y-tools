54b1b0d91c1eb770811d631abc621b18f6291b40
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2023-01-23 10:42:36 -0600

    Comment cleanup

--- tests/get-elements.js
+++ tests/get-elements.js
@@ -5,9 +5,14 @@ javascript: (() => {
     const nodes = root.querySelectorAll("*");
 
     for (const node of nodes) {
+      // If the node is a document fragment, get its host element.
       const element =
         node.nodeType === Node.DOCUMENT_FRAGMENT_NODE ? node.getRootNode().host : node;
-      elements.push(element);
+
+      // If this is truly an element, add it to the elements[] array.
+      if (element.nodeType === Node.ELEMENT_NODE) {
+        elements.push(element);
+      }
 
       // If the node has a shadowRoot, get the nodes within it.
       if (node.shadowRoot) {
