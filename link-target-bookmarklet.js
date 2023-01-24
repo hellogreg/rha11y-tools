db8e9d24463949336a0db3a0c32a65f7c00fabf4
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2023-01-24 15:45:25 -0600

    cleanup of unnecessary files

--- link-target-bookmarklet.js
+++
@@ -1,5 +0,0 @@
-javascript: (function () {
-  var booklink = document.createElement("script");
-  booklink.setAttribute("src", "https://rha11y-tools.netlify.app/link-target.js");
-  document.body.appendChild(booklink);
-})();
--- media-reduced-motion-bookmarklet.js
+++
@@ -1,5 +0,0 @@
-javascript: (function () {
-  var bookletCode = document.createElement("script");
-  bookletCode.setAttribute("src", "http://rha11y-tools.netlify.app/media-reduced-motion.js");
-  document.body.appendChild(bookletCode);
-})();
