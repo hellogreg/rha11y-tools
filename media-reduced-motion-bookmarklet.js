97d8ad4abcb61f02b30b41e89504ab4c20ffbf6b
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2023-01-24 15:37:48 -0600

    new bookmarklet to see if reduced motion is on

---
+++ media-reduced-motion-bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () {
+  var bookletCode = document.createElement("script");
+  bookletCode.setAttribute("src", "http://rha11y-tools.netlify.app/media-reduced-motion.js");
+  document.body.appendChild(bookletCode);
+})();
--- media-reduced-motion.js
+++ media-reduced-motion.js
@@ -1,16 +1,36 @@
-// Script to test if user has reduced motion preference on.
+javascript: (() => {
+  // https://dev.to/natclark/checking-for-reduced-motion-preference-in-javascript-4lp9
+  // https://webkit.org/blog-files/prefers-reduced-motion/prm.htm
 
-// https://dev.to/natclark/checking-for-reduced-motion-preference-in-javascript-4lp9
-// https://webkit.org/blog-files/prefers-reduced-motion/prm.htm
+  function checkMotionPreference() {
+    const prefersReduced =
+      window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
+      window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
 
-/*
+    return prefersReduced;
+  }
 
-const isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+  function outputResults(prefersReduced) {
+    const dialog = document.createElement("dialog");
+    document.body.appendChild(dialog);
 
-if (!!isReduced) {
-    // DON'T use an animation here!
-} else {
-    // DO use an animation here!
-}
+    let h2, p;
 
-*/
+    h2 = document.createElement("h2");
+    h2.appendChild(document.createTextNode("Your current motion preference"));
+    dialog.appendChild(h2);
+
+    p = document.createElement("p");
+    p.appendChild(document.createTextNode("prefers-reduced-motion on: " + prefersReduced));
+    dialog.appendChild(p);
+
+    p = document.createElement("p");
+    p.appendChild(document.createTextNode("Press [esc] or refresh to close"));
+    dialog.appendChild(p);
+
+    dialog.showModal();
+  }
+
+  const prefersReducedMotion = checkMotionPreference();
+  outputResults(prefersReducedMotion);
+})();
