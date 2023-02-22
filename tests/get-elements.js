1d9401ff62704eca87bc7bead912286b5cfc5e16
Greg Gibson <642695+hellogreg@users.noreply.github.com>
2023-02-22 09:01:31 -0600

    text tweaks

--- tests/get-elements.js
+++ tests/get-elements.js
@@ -1,30 +1,80 @@
 javascript: (() => {
-  function getElements(root) {
-    const nodes = root.querySelectorAll("*");
+  //////////////////////////////////////
+  //
+  // CONSOLE HELPER FUNCTIONS
+  //
 
-    for (const node of nodes) {
-      let element;
+  // Custom log() and dir() functions, so we don't have to prepend with console
+  //
+  function log(m) {
+    m = m !== undefined ? m : "";
+    console.log(m);
+  }
 
-      if (node.nodeType === Node.DOCUMENT_FRAGMENT_NODE) {
-        element = node.getRootNode().host;
-      } else {
-        element = node;
+  function dir(m) {
+    if (m) {
+      console.dir(m);
+    }
+  }
+  //
+  //////////////////////////////////////
+
+  //////////////////////////////////////
+  //
+  // APP FUNCTIONS
+  //
+
+  function getAllElements(root) {
+    let elements = [];
+
+    function pushElements(currentRoot) {
+      const nodes = currentRoot.querySelectorAll("*");
+
+      for (const node of nodes) {
+        let element;
+
+        if (node.nodeType === Node.DOCUMENT_FRAGMENT_NODE) {
+          element = node.getRootNode().host;
+        } else {
+          element = node;
+        }
+
+        if (element.nodeType === Node.ELEMENT_NODE) {
+          elements.push(element);
+        }
+
+        // If node has shadowRoot, re-call this function to get its child nodes.
+        if (node.shadowRoot) {
+          pushElements(node.shadowRoot);
+        }
       }
+    }
 
-      if (element.nodeType === Node.ELEMENT_NODE) {
-        //
-        // DO WHATEVER WE NEED TO DO WITH EACH ELEMENT:
-        // PUSH TO AN ARRAY, ETC.
-        //
-        console.dir(element);
-      }
+    pushElements(root);
+    return elements;
+  }
 
-      // If node has shadowRoot, re-call this function to get its child nodes.
-      if (node.shadowRoot) {
-        getElements(node.shadowRoot);
-      }
+  function outputElements(elements) {
+    for (const el of elements) {
+      dir(el);
     }
   }
 
-  getElements(document.body);
+  //
+  //////////////////////////////////////
+
+  //////////////////////////////////////
+  //
+  // INIT FUNCTION
+  //
+
+  (function init() {
+    //
+    // The root can be the document, its body, or whatever we want to test.
+    const root = document.body;
+    const elements = getAllElements(root);
+
+    // Once we have the elements, we can run our tests on them.
+    outputElements(elements);
+  })();
 })();
