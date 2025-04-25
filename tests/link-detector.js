300e263e48060b09a04235f25edbe5ab31318518
hellogreg <booksmart@gmail.com>
2025-04-25 16:02:12 -0500

    first pass at link detector

---
+++ tests/link-detector.js
@@ -0,0 +1,75 @@
+javascript: (() => {
+  //
+  // outputMessages toggles whether log() and dir() output anything
+  let outputMessagesDefault = true;
+  let outputMessages = outputMessagesDefault;
+
+  // Custom log() and dir() functions, so we don't have to prepend with console
+  //
+  function log(m) {
+    if (outputMessages) {
+      m = m !== undefined ? m : " ";
+      console.log(m);
+    }
+  }
+
+  function dir(m) {
+    if (outputMessages && m) {
+      console.dir(m);
+    }
+  }
+
+  function isLink(element) {
+    return element.nodeName.toLowerCase() === "a" && !!element.href;
+  }
+
+  // Returns an element we can use, whether in the shadow DOM or not
+  function getElement(element) {
+    element =
+      element.nodeType === Node.DOCUMENT_FRAGMENT_NODE ? element.getRootNode().host : element;
+    return element;
+  }
+
+  function outputResults(element, getsUnderline) {
+    // Outline the image with the pass/fail color.
+    // (Must reset filters on image, too, to ensure proper outlining)
+    //
+    const colorPass = "#09fb";
+    const colorFail = "#f90b";
+    const outlineColor = !!getsUnderline ? colorPass : colorFail;
+    element.style.setProperty("outline", outlineColor + " solid 8px", "important");
+    element.style.setProperty("outline-offset", "-4px", "important");
+    element.style.setProperty("border-radius", "2px", "important");
+    element.style.setProperty("filter", "initial", "important");
+  }
+
+  function testLinkTargets(root) {
+    const nodes = root.querySelectorAll("*");
+
+    for (const node of nodes) {
+      const element = getElement(node);
+
+      if (isLink(element)) {
+        log("Link: " + element.textContent);
+        log("Link href: " + element.href);
+
+        const getsUnderline = element.closest("p");
+        log("Link is in paragraph: " + !!getsUnderline);
+
+        outputResults(element, getsUnderline);
+        log();
+      }
+
+      // If the node has shadowRoot, re-run this function for it.
+      if (!!node.shadowRoot) {
+        const shadowNode = node.shadowRoot;
+        testLinkTargets(shadowNode);
+      }
+    }
+  }
+
+  (function init() {
+    const root = document.body;
+    testLinkTargets(root);
+  })();
+})();
